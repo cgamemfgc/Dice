0d268fc13ef1d76fa931815c9cfcc37c8f0413c1
cgamemfgc <cgamemfgc@gmail.com>
2024-11-07 23:35:18 +0900

    新規作成

--- Dice/ContentView.swift
+++ Dice/ContentView.swift
@@ -8,12 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var randomNumber = 1
+    @State var timer: Timer?
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Image(systemName:  "die.face.\(randomNumber)")
+                .resizable()
+                .scaledToFit()
+                .frame(width: UIScreen.main.bounds.width/2)
+                .padding()
+            Spacer()
+            Button{
+                print("ボタンが押されたよ")
+                timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true){ _ in
+                    randomNumber = Int.random(in: 1...6)
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5){
+                        timer?.invalidate()
+                        timer = nil
+                    }
+                }
+            } label: {
+                Text("サイコロを振る").padding()
+                    .background(Color.orange)
+                    .foregroundColor(.black)
+                    .cornerRadius(10)
+            }
+            Spacer()
         }
         .padding()
     }
