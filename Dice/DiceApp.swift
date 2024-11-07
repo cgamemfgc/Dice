60a10bb6feac65fc2222e6b513ed19f25fd5edcd
cgamemfgc <cgamemfgc@gmail.com>
2024-11-07 23:03:10 +0900

    Initial Commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by Takeshi Sakamoto on 2024/11/07.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiceTests/DiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiceTests.swift
+//  DiceTests
+//
+//  Created by Takeshi Sakamoto on 2024/11/07.
+//
+
+import Testing
+@testable import Dice
+
+struct DiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
